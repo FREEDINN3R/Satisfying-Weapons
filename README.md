0f73bcea3cf949b773ccf48d322b6440d0043b08
Phill77715 <freedinn3r@gmail.com>
2024-10-11 21:04:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Satisfying Weapons
+ Throw black holes, summon explosions, celebrate birthday parties, and other satisfying ways to destroy your enemies
---
+++ src/main/java/net/freedinner/satisfying_weapons/SatisfyingWeaponsClient.java
@@ -0,0 +1,14 @@
+package net.freedinner.satisfying_weapons;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SatisfyingWeaponsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
