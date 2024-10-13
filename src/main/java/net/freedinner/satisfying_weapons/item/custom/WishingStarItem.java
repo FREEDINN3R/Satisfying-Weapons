28ffd1ae66ee2e3cd34031de4f35d349d3e1de16
Phill77715 <freedinn3r@gmail.com>
2024-10-13 21:31:53 +0200

    Added random drops to wishing star

--- src/main/java/net/freedinner/satisfying_weapons/item/custom/WishingStarItem.java
+++ src/main/java/net/freedinner/satisfying_weapons/item/custom/WishingStarItem.java
@@ -1,12 +1,22 @@
 package net.freedinner.satisfying_weapons.item.custom;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.LootTables;
+import net.minecraft.loot.context.LootContextParameterSet;
+import net.minecraft.loot.context.LootContextParameters;
+import net.minecraft.loot.context.LootContextTypes;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class WishingStarItem extends Item {
     public WishingStarItem(Settings settings) {
@@ -15,9 +25,18 @@ public class WishingStarItem extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack itemStack = user.getStackInHand(hand);
-        user.setStackInHand(hand, new ItemStack(Items.NETHERITE_INGOT));
+        if (world.isClient) {
+            return TypedActionResult.consume(user.getStackInHand(hand));
+        }
 
-        return TypedActionResult.success(itemStack);
+        List<Identifier> allLootTables = LootTables.getAll().stream().filter(chest -> chest.getPath().contains("chests/")).toList();
+        Identifier randomId = allLootTables.get(world.getRandom().nextInt(allLootTables.size()));
+        LootTable lootTable = world.getServer().getLootManager().getLootTable(randomId);
+
+        ObjectArrayList<ItemStack> items = lootTable.generateLoot(new LootContextParameterSet.Builder((ServerWorld) world).add(LootContextParameters.ORIGIN, Vec3d.ZERO).build(LootContextTypes.CHEST));
+        ItemStack randomItemStack = items.get(world.getRandom().nextInt(items.size()));
+        user.setStackInHand(hand, randomItemStack);
+
+        return TypedActionResult.success(user.getStackInHand(hand));
     }
 }
