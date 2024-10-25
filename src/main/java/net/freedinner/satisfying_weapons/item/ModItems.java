755be55feb3705e55c9254e0b70e9e4f2c40fc3c
Phill77715 <freedinn3r@gmail.com>
2024-10-25 20:09:48 +0200

    added custom tool material

--- src/main/java/net/freedinner/satisfying_weapons/item/ModItems.java
+++ src/main/java/net/freedinner/satisfying_weapons/item/ModItems.java
@@ -4,19 +4,21 @@ import net.freedinner.satisfying_weapons.SatisfyingWeapons;
 import net.freedinner.satisfying_weapons.item.custom.FireworkSwordItem;
 import net.freedinner.satisfying_weapons.item.custom.WishingStarItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Rarity;
 
 public class ModItems {
+    private static final int SWORD_DAMAGE = 3;
+    private static final int AXE_DAMAGE = 5;
+
     public static final Item UNFULFILLED_WISH = register("unfulfilled_wish",
             new Item(new Item.Settings().rarity(Rarity.RARE).fireproof()));
     public static final Item WISHING_STAR = register("wishing_star",
             new WishingStarItem(new Item.Settings().maxCount(1).rarity(Rarity.EPIC).fireproof()));
 
     public static final Item FIREWORK_SWORD = register("firework_sword",
-            new FireworkSwordItem(ToolMaterials.IRON, 4, -2.4f, new Item.Settings()));
+            new FireworkSwordItem(ModToolMaterial.RARE, SWORD_DAMAGE, -2.4f, new Item.Settings()));
 
     private static Item register(String name, Item item) {
         return Registry.register(Registries.ITEM, SatisfyingWeapons.id(name), item);
---
+++ src/main/java/net/freedinner/satisfying_weapons/item/ModToolMaterial.java
@@ -0,0 +1,53 @@
+package net.freedinner.satisfying_weapons.item;
+
+import net.fabricmc.yarn.constants.MiningLevels;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public enum ModToolMaterial implements ToolMaterial {
+    RARE(512, 6.0f, 3.0f, 5),
+    EPIC(1024, 7.0f, 4.0f, 12),
+    LEGENDARY(2048, 8.0f, 5.0f, 15);
+
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+
+    private ModToolMaterial(int itemDurability, float miningSpeed, float attackDamage, int enchantability) {
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+    }
+
+    @Override
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return MiningLevels.DIAMOND;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(ModItems.UNFULFILLED_WISH);
+    }
+}
