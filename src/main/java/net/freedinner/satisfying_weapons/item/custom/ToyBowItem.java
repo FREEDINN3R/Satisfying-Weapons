a7ff0febc1466558a282c4e59a537aea8fd6f357
Phill77715 <freedinn3r@gmail.com>
2024-11-23 14:40:55 +0100

    added desc for Toy Bow

--- src/main/java/net/freedinner/satisfying_weapons/item/custom/ToyBowItem.java
+++ src/main/java/net/freedinner/satisfying_weapons/item/custom/ToyBowItem.java
@@ -2,6 +2,8 @@ package net.freedinner.satisfying_weapons.item.custom;
 
 import net.freedinner.satisfying_weapons.entity.custom.ToyArrowEntity;
 import net.freedinner.satisfying_weapons.util.PitchUtils;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.LivingEntity;
@@ -14,12 +16,47 @@ import net.minecraft.item.ToolMaterial;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class ToyBowItem extends UpgradeableBowItem {
     public ToyBowItem(ToolMaterial toolMaterial, Settings settings, int level, @Nullable UpgradeableBowItem nextLevelWeapon) {
         super(toolMaterial, settings, level, nextLevelWeapon);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        super.appendTooltip(stack, world, tooltip, context);
+
+        if (Screen.hasShiftDown()) switch (this.getLevel()) {
+            case 1:
+                tooltip.add(Text.literal("Shoots Toy Arrows, which apply Birthday Party").formatted(Formatting.GRAY));
+                tooltip.add(Text.literal("for 10s on hit. During Birthday Party, the mob will").formatted(Formatting.GRAY));
+                tooltip.add(Text.literal("continuously taunt surrounding mobs to attack them").formatted(Formatting.GRAY));
+                break;
+            case 2:
+                tooltip.add(Text.literal("TODO 2").formatted(Formatting.GRAY));
+                break;
+            case 3:
+                tooltip.add(Text.literal("When applying Birthday Party with Toy Arrow, summon").formatted(Formatting.GRAY));
+                tooltip.add(Text.literal("Birthday Gift above the entity's head. After Birthday").formatted(Formatting.GRAY));
+                tooltip.add(Text.literal("Party ends, or entity dies, Birthday Gift falls on the").formatted(Formatting.GRAY));
+                tooltip.add(Text.literal("ground and explodes, damaging surrounding entities").formatted(Formatting.GRAY));
+                break;
+            case 4:
+                tooltip.add(Text.literal("TODO 4").formatted(Formatting.GRAY));
+                break;
+            case 5:
+                tooltip.add(Text.literal("Hitting a Birthday Gift with a Toy Arrow will detonate").formatted(Formatting.GRAY));
+                tooltip.add(Text.literal("it, causing it to explode and fire off 5 identical").formatted(Formatting.GRAY));
+                tooltip.add(Text.literal("Toy Arrows randomly. These arrows will prioritize hitting").formatted(Formatting.GRAY));
+                tooltip.add(Text.literal("other Gifts, and other mobs without Birthday Party.").formatted(Formatting.GRAY));
+                break;
+        }
     }
 
     @Override
@@ -43,7 +80,7 @@ public class ToyBowItem extends UpgradeableBowItem {
         int useTime = this.getMaxUseTime(stack) - remainingUseTicks;
         float pullProgress = getPullProgress(useTime);
 
-        // Ha ha you are weak
+        // Not enough pull = no shooting
         if (pullProgress < 0.1) {
             return;
         }
@@ -59,19 +96,19 @@ public class ToyBowItem extends UpgradeableBowItem {
                 toyArrow.setCritical(true);
             }
 
-            // Crush them
+            // Power enchantment
             int powerLevel = EnchantmentHelper.getLevel(Enchantments.POWER, stack);
             if (powerLevel > 0) {
                 toyArrow.setDamage(toyArrow.getDamage() + powerLevel * 0.5 + 0.5);
             }
 
-            // Punch them
+            // Punch enchantment
             int punchLevel = EnchantmentHelper.getLevel(Enchantments.PUNCH, stack);
             if (punchLevel > 0) {
                 toyArrow.setPunch(punchLevel);
             }
 
-            // Burn them
+            // Flame enchantment
             if (EnchantmentHelper.getLevel(Enchantments.FLAME, stack) > 0) {
                 toyArrow.setOnFireFor(100);
             }
