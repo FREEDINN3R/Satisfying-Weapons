bb115b5cd57c28ffa853698228bdb5d664ebca0a
Phill77715 <freedinn3r@gmail.com>
2025-01-30 01:15:39 +0100

    gift explosion no longer destroys mob loot

---
+++ src/main/java/net/freedinner/satisfying_weapons/mixin/ExplosionMixin.java
@@ -0,0 +1,24 @@
+package net.freedinner.satisfying_weapons.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.freedinner.satisfying_weapons.util.GiftExplosionBehavior;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ExperienceOrbEntity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.world.explosion.Explosion;
+import net.minecraft.world.explosion.ExplosionBehavior;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(Explosion.class)
+public abstract class ExplosionMixin {
+    @Shadow @Final private ExplosionBehavior behavior;
+
+    @WrapOperation(method = "collectBlocksAndDamageEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isImmuneToExplosion()Z"))
+    private boolean preventLootDestroyedByGift(Entity entity, Operation<Boolean> original) {
+        return original.call(entity) || (behavior instanceof GiftExplosionBehavior && (entity instanceof ItemEntity || entity instanceof ExperienceOrbEntity));
+    }
+}
