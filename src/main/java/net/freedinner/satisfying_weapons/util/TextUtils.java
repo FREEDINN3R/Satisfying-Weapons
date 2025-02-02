cd6ec5bd9d516670b0db422838b13ef9e6e950fc
Phill77715 <freedinn3r@gmail.com>
2025-02-02 16:18:54 +0100

    added brackets check for tooltip desc

--- src/main/java/net/freedinner/satisfying_weapons/util/TextUtils.java
+++ src/main/java/net/freedinner/satisfying_weapons/util/TextUtils.java
@@ -2,7 +2,6 @@ package net.freedinner.satisfying_weapons.util;
 
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +9,19 @@ import java.util.List;
 public class TextUtils {
     public static final int MAX_LINE_LENGTH = 32;
 
+    /* MY CUSTOM SYNTAX FOR DESCRIPTIONS
+          $ = new line
+          _ = non-breaking space
+          {-xxx} = set text color, accepts any int
+     */
+
     public static List<MutableText> breakDownLongTooltip(Text tooltip) {
         String text = tooltip.getString();
+
+        // BRACKETS
+        if (!checkBrackets(text)) {
+            throw new RuntimeException("Encountered tooltip with an incorrect brackets placement");
+        }
 
         String[] words = text.split("\\s+");
         List<String> lines = new ArrayList<>();
@@ -44,5 +54,47 @@ public class TextUtils {
         lines.replaceAll(String::trim);
 
         return lines.stream().map(Text::literal).toList();
+    }
+
+    private static boolean checkBrackets(String text) {
+        // Pos of opening & closing brackets
+        List<Integer> opb = findAllOccurrences(text, "{");
+        List<Integer> clb = findAllOccurrences(text, "}");
+
+        // Should come in pairs
+        if (opb.size() != clb.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < opb.size(); i++) {
+            // Closing bracket must be >1 pos ahead
+            if (clb.get(i) < opb.get(i) + 2) {
+                return false;
+            }
+
+            // Next pair, if exists, must be ahead
+            if (i + 1 < opb.size() && opb.get(i + 1) < clb.get(i) + 1) {
+                return false;
+            }
+        }
+
+        // All checks successful
+        return true;
+    }
+
+    private static List<Integer> findAllOccurrences(String text, String substr) {
+        List<Integer> list = new ArrayList<>();
+        int startingIndex = 0;
+
+        while (startingIndex != -1) {
+            startingIndex = text.indexOf(substr, startingIndex);
+
+            if (startingIndex != -1) {
+                list.add(startingIndex);
+                startingIndex += 1;
+            }
+        }
+
+        return list;
     }
 }
