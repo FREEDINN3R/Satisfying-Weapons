53927bf55ecf861cf8ad2c4bcea1ef6ba9af4d77
Phill77715 <freedinn3r@gmail.com>
2025-06-12 18:59:56 +0200

    implemented lvl 5 black hole hp sacrifice

---
+++ src/main/java/net/freedinner/satisfying_weapons/mixin/ClientPlayerInteractionManagerMixin.java
@@ -0,0 +1,29 @@
+package net.freedinner.satisfying_weapons.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.freedinner.satisfying_weapons.item.ModItems;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public abstract class ClientPlayerInteractionManagerMixin {
+    @ModifyExpressionValue(method = "method_41929", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/ItemCooldownManager;isCoolingDown(Lnet/minecraft/item/Item;)Z"))
+    private boolean summonBlackHoleForHP(boolean isOnCooldown, @Local(argsOnly = true) PlayerEntity player, @Local ItemStack itemStack, @Local(argsOnly = true) Hand hand) {
+        if (!isOnCooldown) {
+            return false;
+        }
+
+        boolean canSacrificeHP = itemStack.isOf(ModItems.SWORD_OF_DYING_STAR.get(4))
+                && hand == Hand.MAIN_HAND
+                && player.getHealth() > 4
+                && player.hurtTime <= 0;
+
+        return !canSacrificeHP;
+    }
+}
---
+++ src/main/java/net/freedinner/satisfying_weapons/mixin/ServerPlayerInteractionManagerMixin.java
@@ -0,0 +1,37 @@
+package net.freedinner.satisfying_weapons.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.freedinner.satisfying_weapons.item.ModItems;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.network.ServerPlayerInteractionManager;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ServerPlayerInteractionManager.class)
+public abstract class ServerPlayerInteractionManagerMixin {
+    @ModifyExpressionValue(method = "interactItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/ItemCooldownManager;isCoolingDown(Lnet/minecraft/item/Item;)Z"))
+    private boolean summonBlackHoleForHP(boolean isOnCooldown, @Local(argsOnly = true) ServerPlayerEntity player, @Local(argsOnly = true) ItemStack itemStack, @Local(argsOnly = true) Hand hand) {
+        if (!isOnCooldown) {
+            return false;
+        }
+
+        boolean canSacrificeHP = itemStack.isOf(ModItems.SWORD_OF_DYING_STAR.get(4))
+                && hand == Hand.MAIN_HAND
+                && player.getHealth() > 4
+                && player.hurtTime <= 0;
+
+        if (canSacrificeHP && !player.isInvulnerable() && !player.isCreative()) {
+            player.damage(player.getWorld().getDamageSources().outOfWorld(), 4);
+
+            if (player.getHealth() <= 1) {
+                player.setHealth(1);
+            }
+        }
+
+        return !canSacrificeHP;
+    }
+}
