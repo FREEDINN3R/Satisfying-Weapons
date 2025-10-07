733d563a7233493dbacce43dfa696f57ba1d3dd7
Phill77715 <freedinn3r@gmail.com>
2025-10-07 19:50:58 +0200

    AMONGUS

--- src/main/java/net/freedinner/satisfying_weapons/event/ModEvents.java
+++ src/main/java/net/freedinner/satisfying_weapons/event/ModEvents.java
@@ -1,10 +1,8 @@
 package net.freedinner.satisfying_weapons.event;
 
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.freedinner.satisfying_weapons.SatisfyingWeapons;
 import net.freedinner.satisfying_weapons.event.custom.CustomLivingEntityEvents;
-import net.freedinner.satisfying_weapons.event.handler.BrokenSoulServerSync;
 import net.freedinner.satisfying_weapons.event.handler.GlassCutAfterDamage;
 import net.freedinner.satisfying_weapons.event.handler.GlassSwordPreventDeath;
 
@@ -14,7 +12,6 @@ public class ModEvents {
 
         ServerLivingEntityEvents.ALLOW_DEATH.register(new GlassSwordPreventDeath());
         CustomLivingEntityEvents.AFTER_DAMAGE.register(new GlassCutAfterDamage());
-        ServerTickEvents.END_SERVER_TICK.register(new BrokenSoulServerSync());
     }
 
     public static void registerEventsClient() {
