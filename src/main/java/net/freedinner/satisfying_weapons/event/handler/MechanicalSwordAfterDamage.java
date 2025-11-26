d8a6792f473b103cb911f4b91cd2743deccebecb
Phill77715 <freedinn3r@gmail.com>
2025-11-26 23:40:13 +0100

    fixed double projectile bug with MS

--- src/main/java/net/freedinner/satisfying_weapons/event/handler/MechanicalSwordAfterDamage.java
+++ src/main/java/net/freedinner/satisfying_weapons/event/handler/MechanicalSwordAfterDamage.java
@@ -1,19 +1,12 @@
 package net.freedinner.satisfying_weapons.event.handler;
 
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.freedinner.satisfying_weapons.event.custom.CustomLivingEntityEvents;
 import net.freedinner.satisfying_weapons.item.custom.MechanicalSwordItem;
-import net.freedinner.satisfying_weapons.mixin.LivingEntityAccessor;
-import net.freedinner.satisfying_weapons.networking.ModNetworking;
 import net.freedinner.satisfying_weapons.util.MathUtils;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
+import net.minecraft.registry.tag.DamageTypeTags;
 import net.minecraft.util.Hand;
 
 public class MechanicalSwordAfterDamage implements CustomLivingEntityEvents.AfterDamage {
@@ -22,6 +15,11 @@ public class MechanicalSwordAfterDamage implements CustomLivingEntityEvents.Afte
         // Only for players who were charging Mechanical Sword of Level 3-5
         if (!(entity.getStackInHand(Hand.MAIN_HAND).getItem() instanceof MechanicalSwordItem mechanicalSword)
                 || !(entity instanceof PlayerEntity player) || !player.isUsingItem() || mechanicalSword.getLevel() < 3) {
+            return;
+        }
+
+        // Prevents bonus charge from own explosion
+        if (source.getAttacker() == entity && source.isIn(DamageTypeTags.IS_EXPLOSION)) {
             return;
         }
 
