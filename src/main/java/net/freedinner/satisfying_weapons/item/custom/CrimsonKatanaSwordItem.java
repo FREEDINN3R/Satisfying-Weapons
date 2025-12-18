e065c5ea56127c98ab8a83e517cd04cc53badae1
Phill77715 <freedinn3r@gmail.com>
2025-12-18 21:31:35 +0300

    implemented katana level 2

--- src/main/java/net/freedinner/satisfying_weapons/item/custom/CrimsonKatanaSwordItem.java
+++ src/main/java/net/freedinner/satisfying_weapons/item/custom/CrimsonKatanaSwordItem.java
@@ -24,38 +24,50 @@ public class CrimsonKatanaSwordItem extends UpgradeableSwordItem {
 
         World world = target.getWorld();
 
+        float poisonDamage = 0, witherDamage = 0, burnDamage = 0;
+        int effectCount = 0;
+
         // Poison goes first because it's non-lethal
         StatusEffectInstance poison = target.getStatusEffect(StatusEffects.POISON);
         if (poison != null && this.getLevel() >= 2) {
             int[] dmgRate = {25, 12, 6, 3, 1};
             int amplifier = Math.min(4, poison.getAmplifier());
-            float totalDamage = poison.getDuration() / dmgRate[amplifier];
-            totalDamage = Math.max(0, (Math.min(totalDamage, target.getHealth() - 1)));
+            poisonDamage = poison.getDuration() / dmgRate[amplifier];
+            poisonDamage = Math.max(0, (Math.min(poisonDamage, target.getHealth() - 1)));
 
-            attacker.sendMessage(Text.literal("Detonating poison for " + totalDamage + " damage"));
-            target.damage(CombatHelper.getDamageSource(ModDamageTypes.INSTANT_POISON, world), totalDamage);
             target.removeStatusEffect(StatusEffects.POISON);
+            effectCount++;
         }
 
         StatusEffectInstance wither = target.getStatusEffect(StatusEffects.WITHER);
         if (wither != null) {
             int[] dmgRate = {40, 20, 10, 5, 2, 1};
             int amplifier = Math.min(5, wither.getAmplifier());
-            int totalDamage = wither.getDuration() / dmgRate[amplifier];
+            witherDamage = wither.getDuration() / dmgRate[amplifier];
 
-            attacker.sendMessage(Text.literal("Detonating wither for " + totalDamage + " damage"));
-            target.damage(CombatHelper.getDamageSource(ModDamageTypes.INSTANT_WITHER, world), totalDamage);
             target.removeStatusEffect(StatusEffects.WITHER);
+            effectCount++;
         }
 
         if (target.isOnFire() && this.getLevel() >= 4) {
             int dmgRate = 20;
-            int totalDamage = target.getFireTicks() / dmgRate;
+            burnDamage = target.getFireTicks() / dmgRate;
 
-            attacker.sendMessage(Text.literal("Detonating burn for " + totalDamage + " damage"));
-            target.damage(CombatHelper.getDamageSource(ModDamageTypes.INSTANT_BURN, world), totalDamage);
             target.setFireTicks(0);
+            effectCount++;
         }
+
+        if (effectCount >= 2 && this.getLevel() >= 2) {
+            poisonDamage *= 1.2f;
+            witherDamage *= 1.2f;
+            burnDamage *= 1.2f;
+        }
+
+        attacker.sendMessage(Text.literal("poison = " + poisonDamage + "; wither = " + witherDamage + "; burn = " + burnDamage));
+
+        target.damage(CombatHelper.getDamageSource(ModDamageTypes.INSTANT_POISON, world), poisonDamage);
+        target.damage(CombatHelper.getDamageSource(ModDamageTypes.INSTANT_WITHER, world), witherDamage);
+        target.damage(CombatHelper.getDamageSource(ModDamageTypes.INSTANT_BURN, world), burnDamage);
 
         return super.postHit(stack, target, attacker);
     }
